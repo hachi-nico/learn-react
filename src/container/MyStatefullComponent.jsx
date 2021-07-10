705fca773cff026baa624c5d2caa0d2913f235f6
hachi-nico <nicokun48@gmail.com>
2021-07-10 12:11:33 +0700

    clean some files

--- src/container/MyStatefullComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class MyStatefullComponent extends React.Component {
-  render() {
-    return <p className="main-text">Hello, this is statefull component (class component)</p>
-  }
-}
-
-export default MyStatefullComponent;
