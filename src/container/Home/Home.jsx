5bff9fd3ed96c04308772cc4a8069a43ab6cf4e4
hachi-nico <nicokun48@gmail.com>
2021-07-10 23:40:13 +0700

    react port changed, fetch data from json-server

--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react'
-import axios from 'axios';
+import axios from 'axios'
 import './Home.css'
 import ProfileCard from '../../component/ProfileCard/ProfileCard'
 // import Counter from '../Counter/Counter'
@@ -17,12 +17,11 @@ class Home extends Component {
     //       profileData: json,
     //     })
     //   })
-    axios.get('https://jsonplaceholder.typicode.com/users')
-      .then((res) => {        
-        this.setState({
-          profileData: res.data,
-        })
+    axios.get('http://localhost:3000/profile').then((res) => {
+      this.setState({
+        profileData: res.data,
       })
+    })
   }
 
   render() {
@@ -31,7 +30,13 @@ class Home extends Component {
         <h1 className='heading'>My Profiles</h1>
         <div className='container'>
           {this.state.profileData.map((profileData) => {
-            return <ProfileCard key={profileData.id} name={profileData.name} desc={profileData.username} />
+            return (
+              <ProfileCard
+                key={profileData.id}
+                name={profileData.name}
+                desc={profileData.desc}
+              />
+            )
           })}
         </div>
         {/* <div>
