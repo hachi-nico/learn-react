6466cf5ae6c71eaa20fdb4fe6fe9a31dac0e84fa
hachi-nico <nicokun48@gmail.com>
2021-07-10 12:11:22 +0700

    clean some files

--- src/component/MyStatelessComponent.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './MyStatelessComponent.css';
-
-const MyStatelessComponent = () => {
-  return <p className="main-text">Hello, this is stateless component (functional)</p>
-}
-
-export default MyStatelessComponent;
