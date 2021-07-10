e25296760c439126fce0c4bc0b44b05e7572e532
hachi-nico <nicokun48@gmail.com>
2021-07-10 23:27:48 +0700

    add json-server

--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -1,17 +1,38 @@
 import React, { Component, Fragment } from 'react'
+import axios from 'axios';
 import './Home.css'
 import ProfileCard from '../../component/ProfileCard/ProfileCard'
 // import Counter from '../Counter/Counter'
 
 class Home extends Component {
+  state = {
+    profileData: [],
+  }
+
+  componentDidMount() {
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    //   .then((response) => response.json())
+    //   .then((json) => {
+    //     this.setState({
+    //       profileData: json,
+    //     })
+    //   })
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then((res) => {        
+        this.setState({
+          profileData: res.data,
+        })
+      })
+  }
+
   render() {
     return (
       <Fragment>
         <h1 className='heading'>My Profiles</h1>
         <div className='container'>
-          <ProfileCard name='nico' desc='Seorang yang jujur' />
-          <ProfileCard name='akbar' desc='Seorang yang baik' />
-          <ProfileCard />
+          {this.state.profileData.map((profileData) => {
+            return <ProfileCard key={profileData.id} name={profileData.name} desc={profileData.username} />
+          })}
         </div>
         {/* <div>
           <Counter />
