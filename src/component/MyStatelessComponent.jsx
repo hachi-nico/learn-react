55483d34eeb4e43b9d1d3cebc574524edbef06b7
hachi-nico <nicokun48@gmail.com>
2021-07-10 11:00:41 +0700

    add new component

---
+++ src/component/MyStatelessComponent.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './MyStatelessComponent.css';
+
+const MyStatelessComponent = () => {
+  return <p className="main-text">Hello, this is stateless component (functional)</p>
+}
+
+export default MyStatelessComponent;
---
+++ src/container/MyStatefullComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class MyStatefullComponent extends React.Component {
+  render() {
+    return <p className="main-text">Hello, this is statefull component (class component)</p>
+  }
+}
+
+export default MyStatefullComponent;
