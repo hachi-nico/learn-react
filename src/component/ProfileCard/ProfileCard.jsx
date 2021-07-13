4a7f1f488ae684561ea8339c333aa147d1e3058f
hachi-nico <nicokun48@gmail.com>
2021-07-13 08:12:03 +0700

    add put and post method

--- src/component/ProfileCard/ProfileCard.jsx
+++ src/component/ProfileCard/ProfileCard.jsx
@@ -4,12 +4,13 @@ import logo from './img/logo192.png'
 
 const ProfileCard = (props) => {
   return (
-    <div className='profile-container'>
-      <img className='logo' src={logo} alt='Logo' />
-      <div className='name'>{props.data.name}</div>
-      <p>{props.data.desc}</p>
-      <button onClick={() => props.remove(props.data.id)}>Remove</button>
-    </div>
+      <div className="profile-container">
+          <img className="logo" src={logo} alt="Logo" />
+          <div className="name">{props.data.name}</div>
+          <p>{props.data.desc}</p>
+          <button onClick={() => props.update(props.data)}>Update</button>
+          <button onClick={() => props.remove(props.data.id)}>Remove</button>
+      </div>
   )
 }
 
--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -2,25 +2,81 @@ import React, { Component, Fragment } from 'react'
 import axios from 'axios'
 import './Home.css'
 import ProfileCard from '../../component/ProfileCard/ProfileCard'
-// import Counter from '../Counter/Counter'
 
 class Home extends Component {
   state = {
     profileData: [],
+    profileForm: {
+      id: 1,
+      name: '',
+      desc: '',
+    },
+    isUpdate: false,
   }
 
-  getProfileAPI = () => {
-    axios.get('http://localhost:3000/profile').then((res) => {
+  getProfileAPI = () => {    
+    axios.get('http://localhost:3000/profile?_sort=id&_order=desc').then((res) => {
       this.setState({
         profileData: res.data,
       })
     })
   }
 
+  putProfileAPI = () => {
+    axios.put(`http://localhost:3000/profile/${this.state.profileForm.id}`, this.state.profileForm)
+      .then(res => {
+        window.alert('berhasil update');
+        this.getProfileAPI()
+      })
+  }
+
+  getInputForm = (event) => {
+    let inputValue = this.state.profileForm;
+    let timestamp = new Date().getTime()
+
+    if(!this.state.isUpdate) {
+      inputValue["id"] = timestamp;  
+    }
+ 
+    inputValue[event.target.name] = event.target.value;
+    
+    this.setState({
+      profileForm: inputValue,
+    })
+  }
+
+  sendInputForm = () => {
+    axios.post('http://localhost:3000/profile', this.state.profileForm)
+      .then(() => {
+        alert('berhasil menambahkan data');
+        this.getProfileAPI();
+      }, (err) => {
+        window.alert('error:', err);
+      })
+  }
+
+  submitBtn = () => {
+    if(this.state.isUpdate) {
+      this.putProfileAPI();
+    } else {
+      this.sendInputForm();
+    }    
+  }
+
   removeBtn = (val) => {
-    axios
-      .delete(`http://localhost:3000/profile/${val}`)
-      .then(() => this.getProfileAPI())
+    const confirmDialog = window.confirm('konfirmasi')
+
+    if (confirmDialog === true) {
+      axios.delete(`http://localhost:3000/profile/${val}`)
+        .then(() => this.getProfileAPI())
+    }
+  }
+
+  updateBtn = (val) => {  
+    this.setState({
+      profileForm: val,
+      isUpdate: true,
+    })    
   }
 
   componentDidMount() {
@@ -30,21 +86,46 @@ class Home extends Component {
   render() {
     return (
       <Fragment>
-        <h1 className='heading'>My Profiles</h1>
-        <div className='container'>
+        <h1 className="heading">Tambah Data</h1>
+        <div className="form-container">
+          <label htmlFor="name">Nama</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="masukkan nama"
+            onChange={this.getInputForm}
+            value={this.state.profileForm.name}
+          />
+
+          <label htmlFor="desc">Deskripsi</label>
+          <textarea
+            name="desc"
+            id="desc"
+            cols="30"
+            rows="10"
+            placeholder="masukkan deskripsi"
+            onChange={this.getInputForm}
+            value={this.state.profileForm.desc}
+          ></textarea>
+
+          <button className="submit-btn" onClick={this.submitBtn}>
+            Tambah
+          </button>
+        </div>
+        <h1 className="heading">My Profiles</h1>
+        <div className="container">
           {this.state.profileData.map((profileData) => {
             return (
               <ProfileCard
                 key={profileData.id}
                 data={profileData}
                 remove={this.removeBtn}
+                update={this.updateBtn}
               />
             )
           })}
         </div>
-        {/* <div>
-          <Counter />
-        </div>        */}
       </Fragment>
     )
   }
